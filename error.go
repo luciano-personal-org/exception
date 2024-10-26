f56a80809e54bde620f09bff3b8baceb1aeaa74e
Luciano Silva <lurodrisilva@hotmail.com>
2024-10-26 20:26:33 -0300

    adjusting error handling

--- error.go
+++ error.go
@@ -26,3 +26,23 @@ func PanicIfNeeded(err interface{}) {
 		panic(err)
 	}
 }
+
+// PanicIfNeeded panics if the given error is not nil.
+func DontPanic(err interface{}) {
+	// Check if the given error is not nil.
+	if err != nil {
+		// Check if the error is of type TradingError.
+		if te, ok := err.(TradingError); ok {
+			// Print the message and code.
+			fmt.Printf("Custom Error: %s, Code: %s\n", te.Error(), te.ErrorCode())
+			// Check if there are details.
+			if te.Details() != "" {
+				fmt.Printf("Details: %s\n", te.Details())
+			}
+			// Check if there is an original error.
+			if te.OriginalError() != nil {
+				fmt.Printf("Original Error: %s\n", te.OriginalError().Error())
+			}
+		}
+	}
+}
